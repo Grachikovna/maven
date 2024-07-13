c45d980a8617607d2c8f436f70e1a56342d39617
origin <maryna.oganesyan@yandex.ru>
2024-07-13 20:08:33 +0400

    wb

--- src/main/java/com/mari/calculator/CalculatorController.java
+++ src/main/java/com/mari/calculator/CalculatorController.java
@@ -1,9 +1,42 @@
 package com.mari.calculator;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
 
+    public final CalculatorService calculatorService;
 
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping
+    public String welcome() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @GetMapping("/plus")
+    public String plus(@RequestParam int a, @RequestParam int b) {
+        return a + " + " + b + " = " + calculatorService.plus(a, b);
+    }
+
+    @GetMapping("/minus")
+    public String minus(@RequestParam int a, @RequestParam int b) {
+        return a + " - " + b + " = " + calculatorService.minus(a, b);
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam int a, @RequestParam int b) {
+        return a + " * " + b + " = " + calculatorService.multiply(a, b);
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam int a, @RequestParam int b) {
+        return a + " / " + b + " = " + calculatorService.divide(a, b);
+    }
 }
